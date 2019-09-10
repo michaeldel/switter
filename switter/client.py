84da20717d726a28fe977c5d9e4479dbaedc291b
michaeldel <michaeldel@protonmail.com>
2019-09-10 07:16:08 +0200

    Fix mypy import errors

--- switter/client.py
+++ switter/client.py
@@ -6,7 +6,7 @@ import urllib.parse
 
 from typing import Iterable, List, Optional, Tuple
 
-from bs4 import BeautifulSoup
+from requests_html import Element, HTML, HTMLSession  # type: ignore
 
 
 _CHROME_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'  # noqa: E501
